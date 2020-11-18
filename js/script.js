3b3c6e7bf19b49dddd9718a44e79d9feb7e7778d
Alecsey <lalecsey@gmail.com>
2020-11-18 15:08:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//let result = confirm('Are you here?');
+//console.log(result);
+
+// let answer = prompt("Вам есть 18", "");
+// console.log(typeof answer);
+
+// let answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// let category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// let user = 'Ivan';
+// alert(`Привет, ${user}`);
+
+ let inc = 10;
+ let decr = 10;
+
+ console.log(inc++);
+ console.log(--decr);
+
+ let isChecked = true;
+ let isClose = true;
+
+ console.log(isChecked && isClose);  //  логическое и 
+
+ console.log(isChecked || isClose);  // логическое или
+
+// == логическое сравнение
+// !=  не равно
