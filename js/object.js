16e1c102352b6d52754b173c8232c66ca7595a8a
Alecsey <lalecsey@gmail.com>
2020-11-19 22:34:30 +0300

    add object

---
+++ js/object.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red',
+    }
+};
+
+console.log(options.name);
+
+delete options.name;
+
+console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) == 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} и значение ${options[key][i]}`);
+            
+        }
+    } else {
+        console.log(`Свойство ${key} и значение ${options[key]}`);
+    }
+    counter++;
+}
+
+console.log(counter);
+console.log(Object.keys(options).length);
+
+
+
