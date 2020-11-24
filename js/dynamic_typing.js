72a217a128b51f01c395a7f82d52e901883d5e57
Alecsey <lalecsey@gmail.com>
2020-11-24 16:24:51 +0300

    add dynamin typing

---
+++ js/dynamic_typing.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//String
+
+console.log(String(null));
+console.log(String(4));
+
+
+console.log(typeof(5 + ''));
+
+let num=5;
+
+console.log('https://vk.com/catalog/' + num);
+
+let fontSize = 26 + 'px';
+
+//Number
+
+console.log(Number('4'));
+
+console.log(+'5');
+
+
+console.log(parseInt('15px', 10));
+
+let answ = +prompt('Hello', '');
+
+// Boolean
+
+let toFalse = [0, '', null, undefined, NaN];
+
+console.log(Boolean('4'));
+console.log(!!'4'); // два знака отрицания
+
+[] + false - null + true;
