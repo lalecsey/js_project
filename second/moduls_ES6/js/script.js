c63f89b2e88d94bffaf88d556711d1dde091e43c
Alecsey <lalecsey@gmail.com>
2021-01-10 19:21:03 +0300

    update project

---
+++ second/moduls_ES6/js/script.js
@@ -0,0 +1,14 @@
+// // import {one as first, two} from './main';
+// import * as data from './main';
+
+
+// //console.log(first);
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
+
+import {one, two} from './main.js';
+import sayHi from './main.js';
+
+//console.log(first);
+console.log(`${one} and ${two}`);
+sayHi();
